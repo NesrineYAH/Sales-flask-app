06be85a0074b571858defbf9770b81fc955a7038
NesrineYAH <nesrineyahoum@gmail.com>
2025-10-21 12:08:51 +0200

    Création automatiquement etl.log après avoir lancer le pipinline avec ventes_jours.csv

--- app/run_etl.py
+++ app/run_etl.py
@@ -1,15 +1,18 @@
-from etl import executer_pipeline
-
-# Lancer le pipeline ETL sur le fichier ventes_jour.csv
-executer_pipeline("./data/ventes_jour.csv")
-
-
-
-
+import os
 import logging
 from etl import executer_pipeline
 
-logging.basicConfig(filename="logs/etl.log", level=logging.INFO, format="%(asctime)s - %(message)s")
-executer_pipeline("../data/ventes_jour.csv")
-logging.info("Pipeline ETL exécuté avec succès")
+# Crée le dossier logs s'il n'existe pas
+os.makedirs("logs", exist_ok=True)
 
+# Config du logging
+
+logging.basicConfig(filename="logs/etl.log", level=logging.INFO, format="%(asctime)s - %(message)s")
+# Fichier CSV à traiter
+
+try:
+    logging.info(f"🚀 Lancement du pipeline ETL pour le fichier {"./data/ventes_jour.csv"}")
+    executer_pipeline("./data/ventes_jour.csv")
+    logging.info("✅ Pipeline ETL exécuté avec succès")
+except Exception as e:
+    logging.error(f"❌ Erreur lors de l'exécution du pipeline : {e}")
